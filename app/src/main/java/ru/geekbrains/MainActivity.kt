7427dbe7c4ff348752bed443d8aff97554e0687e
a.firsov <a.firsov@example.com>
2021-12-06 23:21:05 +0300

    move to viewbinding

--- app/src/main/java/ru/geekbrains/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/MainActivity.kt
@@ -1,9 +1,9 @@
 package ru.geekbrains
 
 import android.os.Bundle
-import android.widget.Button
 import moxy.MvpAppCompatActivity
 import moxy.ktx.moxyPresenter
+import ru.geekbrains.databinding.ActivityMainBinding
 import ru.geekbrains.models.ButtonTypeEnum
 import ru.geekbrains.models.ButtonUiModel
 import ru.geekbrains.mvp.MainPresenter
@@ -11,38 +11,34 @@ import ru.geekbrains.mvp.MainView
 
 class MainActivity : MvpAppCompatActivity(R.layout.activity_main), MainView {
 
-    private var btnCounter1: Button? = null
-    private var btnCounter2: Button? = null
-    private var btnCounter3: Button? = null
-
+    private lateinit var viewBinding: ActivityMainBinding
     private val presenter by moxyPresenter {
         MainPresenter()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        btnCounter1 = findViewById(R.id.btn_counter1)
-        btnCounter2 = findViewById(R.id.btn_counter2)
-        btnCounter3 = findViewById(R.id.btn_counter3)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
-        btnCounter1?.setOnClickListener {
+        viewBinding.btnCounter1.setOnClickListener {
             presenter.counterClick(ButtonTypeEnum.FIRST_BUTTON)
         }
 
-        btnCounter2?.setOnClickListener {
+        viewBinding.btnCounter2.setOnClickListener {
             presenter.counterClick(ButtonTypeEnum.SECOND_BUTTON)
         }
 
-        btnCounter3?.setOnClickListener {
+        viewBinding.btnCounter3.setOnClickListener {
             presenter.counterClick(ButtonTypeEnum.THIRD_BUTTON)
         }
     }
 
     override fun setButtonText(model: ButtonUiModel) {
         when(model.index) {
-            ButtonTypeEnum.FIRST_BUTTON -> btnCounter1?.text = model.value
-            ButtonTypeEnum.SECOND_BUTTON -> btnCounter2?.text = model.value
-            ButtonTypeEnum.THIRD_BUTTON -> btnCounter3?.text = model.value
+            ButtonTypeEnum.FIRST_BUTTON -> viewBinding.btnCounter1.text = model.value
+            ButtonTypeEnum.SECOND_BUTTON -> viewBinding.btnCounter2.text = model.value
+            ButtonTypeEnum.THIRD_BUTTON -> viewBinding.btnCounter3.text = model.value
         }
     }
 }
